729f2a6c10baf111d83e9046e3b69367d9e3cd21
jenlau250 <jlau250@gmail.com>
2019-05-25 21:24:06 -0700

    Connect to DAO

--- src/jCalendar/viewcontroller/DAOLoginController.java
+++ src/jCalendar/viewcontroller/DAOLoginController.java
@@ -16,6 +16,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -30,40 +31,52 @@ import javafx.scene.control.cell.PropertyValueFactory;
  */
 public class DAOLoginController implements Initializable {
 
-    @FXML
-    private TableColumn<?, ?> UserName;
-
-    @FXML
-    private Label labelUserId;
-
-    @FXML
-    private Button buttonLogin;
-
-    @FXML
-    private TextField textUserId;
-
-    @FXML
-    private TableView<User> UserTable;
-
-    @FXML
-    private TextField textUserPw;
-
-    @FXML
-    private TableColumn<?, ?> ID;
-
-    @FXML
-    private TableColumn<?, ?> Password;
-
-    @FXML
-    private Label labelUserPw;
+    @FXML    private TableColumn<?, ?> UserName;
+    @FXML    private Label labelUserId;
+    @FXML    private Button buttonLogin;
+    @FXML    private Button buttonCancel;
+    @FXML    private TextField textUserId;
+    @FXML    private TableView<User> UserTable;
+    @FXML    private TextField textUserPw;
+    @FXML    private TableColumn<?, ?> ID;
+    @FXML    private TableColumn<?, ?> Password;
+    @FXML    private Label labelUserPw;
+    ObservableList<User> Users= FXCollections.observableArrayList();
 
     @FXML
     void handleActionLogin(ActionEvent event) {
+// Show error message If user name or password is blank
+	if ((textUserId.getText().length() == 0) || (textUserPw.getText().length() == 0)) {
+	    Alert alert = new Alert(Alert.AlertType.ERROR);
+	    alert.setTitle("Missing Login Information");
+	    alert.setHeaderText(null);
+	    alert.setContentText("Please enter user name and password to login.");
+	    alert.showAndWait();
 
+	} else {
+	    String userNameInput = textUserId.getText();
+	    String userPwInput = textUserPw.getText();
+
+// Login successful if user name and password matches
+	    boolean validLogin = false;
+	    for (User user : Users) {
+		if ((user.getUserName().equals(userNameInput)) && user.getPassword().equals(userPwInput)) {
+		    validLogin = true;
+		}
+	    }
+
+	    System.out.println(validLogin);
+	}
     }
 
+        
     @FXML
-    ObservableList<User> Users= FXCollections.observableArrayList();
+    void handleActionCancel(ActionEvent event) {
+	System.exit(0);
+    }
+    
+
+    
     /**
      * Initializes the controller class.
      */
@@ -75,6 +88,7 @@ public class DAOLoginController implements Initializable {
 //       CustomerAddress2.setCellValueFactory(new PropertyValueFactory<>("customerAddress2"));
        Password.setCellValueFactory(new PropertyValueFactory<>("password"));
        
+// populate user table from SQL 
 
         try {
             Users.addAll(UserDaoImpl.getAllUsers());
