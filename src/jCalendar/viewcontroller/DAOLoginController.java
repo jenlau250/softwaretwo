9534ab15c358e828fcf07d0e9b7c8e20c5aa0ab5
jenlau250 <jlau250@gmail.com>
2019-05-25 20:05:41 -0700

    Connect to DAO

--- src/jCalendar/viewcontroller/DAOLoginController.java
+++ src/jCalendar/viewcontroller/DAOLoginController.java
@@ -13,28 +13,56 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
  *
- * @author carolyn.sher
+ * @author jlau2
  */
 public class DAOLoginController implements Initializable {
 
     @FXML
+    private TableColumn<?, ?> UserName;
+
+    @FXML
+    private Label labelUserId;
+
+    @FXML
+    private Button buttonLogin;
+
+    @FXML
+    private TextField textUserId;
+
+    @FXML
     private TableView<User> UserTable;
+
+    @FXML
+    private TextField textUserPw;
+
     @FXML
     private TableColumn<?, ?> ID;
-    @FXML
-    private TableColumn<?, ?> UserName;
+
     @FXML
     private TableColumn<?, ?> Password;
 
+    @FXML
+    private Label labelUserPw;
+
+    @FXML
+    void handleActionLogin(ActionEvent event) {
+
+    }
+
+    @FXML
     ObservableList<User> Users= FXCollections.observableArrayList();
     /**
      * Initializes the controller class.
